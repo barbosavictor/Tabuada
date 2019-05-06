f31187038abbb27536a3be2e0ce9f0ae40c4632d
barbosavictor <43385570+barbosavictor@users.noreply.github.com>
2019-05-06 14:56:23 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,10 @@
+let i = 1, j = 1, a = 2
+while(i < 11){
+    document.write("<p> Tabuada do " + i +"</p>")
+    while(j < 11 && a < 11){
+        document.write("<p>"+ i + " x " + j + " = " + (i * j) +"</p>")
+        j++
+    } j = 0
+    
+    i++
+}
